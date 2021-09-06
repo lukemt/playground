634f7f9dd12a5fe9cedc81ed97c7621dc6922ec4
Lukas Petry <lukemt@rennwurst.de>
2021-09-06 16:59:38 +0200

    add solution 08s

---
+++ src/2021_09_06/08s-exercise-colorful-boxes.js
@@ -0,0 +1,37 @@
+// 1. Create colorful boxes from this array with a size of 100 x 100px
+// 2. set the colors from the array
+// 3. set the borderRadius to the value from the array
+// 4. set the content of each element to the given text from the array
+
+const shapes = [
+  {
+    color: 'hotpink',
+    radius: '10px',
+    text: 'Wow'
+  },
+  {
+    color: '#C0FFEE',
+    radius: '30px',
+    text: ''
+  },
+  {
+    color: 'hsl(260deg, 60%, 80%)',
+    radius: '50%'
+  },
+  {
+    color: 'steelblue',
+    radius: '0'
+  },
+]
+
+const body = document.body
+
+shapes.forEach(shape => {
+  const box = document.createElement('div')
+  box.style.width = '100px'
+  box.style.height = '100px'
+  box.style.backgroundColor = shape.color
+  box.style.borderRadius = shape.radius
+  box.innerText = shape.text
+  body.appendChild(box)
+})
