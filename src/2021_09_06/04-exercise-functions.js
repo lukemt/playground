8395cef6d4788b466137116eecb5c746b5ad5eb4
Lukas Petry <lukemt@rennwurst.de>
2021-09-06 13:49:00 +0200

    add exercises

---
+++ src/2021_09_06/04-exercise-functions.js
@@ -0,0 +1,10 @@
+// 1. write a function that multiplies two numbers
+// the function should take two arguments
+// the first argument should be the first number
+// the second argument should be the second number
+// the function should return the product of the two numbers
+
+// example usage: multiply(2, 3) // returns 6
+
+// 2. rewrite the function into 3 different syntaxes
+// see: 01-functions.js
---
+++ src/2021_09_06/05-exercise-arrays-and-objects.js
@@ -0,0 +1,50 @@
+const people = [
+  {
+    name: 'Harry',
+    age: 11,
+    parents: [{ mothersName: 'Lily', fathersName: 'James' }],
+    pet: [{ animal: 'owl', name: 'Hedwig' }],
+    house: [{ name: 'gryffindor', color: 'red' }],
+    'hogwarts favorites': [
+      'quidditch',
+      'defense against the dark arts',
+      'gryffindor common room',
+    ],
+  },
+  {
+    name: 'Ron',
+    age: 10,
+    parents: [{ mothersName: 'Molly', fathersName: 'Arthur' }],
+    pet: [{ animal: 'rat', name: 'Scabbers' }],
+    house: [{ name: 'gryffindor', color: 'red' }],
+  },
+  {
+    name: 'Hermione',
+    age: 12,
+    parents: [{ mothersName: 'Mrs. Granger', fathersName: 'Mr. Granger' }],
+    pet: [{ animal: 'cat', name: 'Crookshanks' }],
+    house: [{ name: 'gryffindor', color: 'red' }],
+  },
+  {
+    name: 'Draco',
+    age: 12,
+    parents: [{ mothersName: 'Narcissa', fathersName: 'Lucius' }],
+    pet: [{ animal: 'eagle owl', name: undefined }],
+    house: [{ name: 'slytherin', color: 'green' }],
+  },
+]
+
+// log out the name of the first person
+
+// log out the hogwarts favourites of the first person
+
+// log out the age of the third person
+
+// log out the mothers name of the second person
+
+// log out the type of animal of the first persons pet
+
+// log out the name of each person
+
+// create a HTMLElement with a class box for each person
+// and change the background of the HTMLElements with the house color
