ac4b123bd15273cd6e11268477f09dafc3af3c05
Lukas Petry <lukemt@rennwurst.de>
2021-09-06 16:45:15 +0200

    add exercise-colorful-boxes.js

---
+++ src/2021_09_06/08-exercise-colorful-boxes.js
@@ -0,0 +1,25 @@
+// 1. Create colorful boxes from this array with a size of 100 x 100px
+// 2. set the colors from the array
+// 3. set the borderRadius to the value from the array
+// 4. set the content of each element to the given text from the array
+
+const shapes = [
+  {
+    color: 'hotpink',
+    radius: '10px',
+    text: 'Wow'
+  },
+  {
+    color: '#C0FFEE',
+    radius: '30px',
+    text: ''
+  },
+  {
+    color: 'hsl(260deg, 60%, 80%)',
+    radius: '50%'
+  },
+  {
+    color: 'steelblue',
+    radius: '0'
+  },
+]
