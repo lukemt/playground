d81e11948a01acb4a4bc0b627a11c24432b52c05
Lukas Petry <lukemt@rennwurst.de>
2021-09-07 12:10:41 +0200

    add filter exercise

---
+++ src/2021_09_07/03-exercise-filter.js
@@ -0,0 +1,59 @@
+// Array methods
+
+const weasleys = [
+  {
+    name: 'Arthur',
+    yearOfBirth: 1950,
+    profession: 'Head of the Misuse of Muggle Artefacts Office',
+  },
+  {
+    name: 'Molly',
+    yearOfBirth: 1950,
+    profession: 'Housewife',
+  },
+  {
+    name: 'Bill',
+    yearOfBirth: 1970,
+    profession: 'Curse-Breaker for Gringotts Bank',
+  },
+  {
+    name: 'Charlie',
+    yearOfBirth: 1972,
+    profession: 'Dragonologist',
+  },
+  {
+    name: 'Percy',
+    yearOfBirth: 1976,
+    profession: 'Student',
+  },
+  {
+    name: 'Fred',
+    yearOfBirth: 1978,
+    profession: 'Student',
+  },
+  {
+    name: 'George',
+    yearOfBirth: 1978,
+    profession: 'Student',
+  },
+  {
+    name: 'Ron',
+    yearOfBirth: 1980,
+    profession: 'Student',
+  },
+  {
+    name: 'Ginny',
+    yearOfBirth: 1981,
+    profession: 'Student',
+  },
+]
+
+// 1. Only show students
+
+// 2. Only show thode who are not students
+
+// 3. Only show the ones born before 1980
+
+// 4. Only show persons with names starting with 'G'
+
+// 5. Only show persons with names ending with 'y'
