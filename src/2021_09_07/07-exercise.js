2aa148d268014544312bc32a448cb8998741124e
Lukas Petry <lukemt@rennwurst.de>
2021-09-07 15:52:56 +0200

    add exercise.js

---
+++ src/2021_09_07/07-exercise.js
@@ -0,0 +1,58 @@
+// Array methods
+
+const weasleys = [
+  {
+    name: 'Arthur',
+    yearOfBirth: 1950,
+    profession: 'Head of the Misuse of Muggle Artefacts Office',
+  },
+  {
+    name: 'Molly',
+    yearOfBirth: 1950,
+    profession: 'Housewife',
+  },
+  {
+    name: 'Bill',
+    yearOfBirth: 1970,
+    profession: 'Curse-Breaker for Gringotts Bank',
+  },
+  {
+    name: 'Charlie',
+    yearOfBirth: 1972,
+    profession: 'Dragonologist',
+  },
+  {
+    name: 'Percy',
+    yearOfBirth: 1976,
+    profession: 'Student',
+  },
+  {
+    name: 'Fred',
+    yearOfBirth: 1978,
+    profession: 'Student',
+  },
+  {
+    name: 'George',
+    yearOfBirth: 1978,
+    profession: 'Student',
+  },
+  {
+    name: 'Ron',
+    yearOfBirth: 1980,
+    profession: 'Student',
+  },
+  {
+    name: 'Ginny',
+    yearOfBirth: 1981,
+    profession: 'Student',
+  },
+]
+// Create a new array with names only
+
+// Create a new array with students only
+
+// Check if 'Bill' is a student (assume you do not know the position/index of Bill in the array)
+
+// Sort the Weasleys by age ascending
+
+// Sort the Weasleys by name
